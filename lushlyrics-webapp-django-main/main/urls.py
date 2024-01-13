28e5169c2fef03bd7605cf9de1b055ce1a3c61ec
Hadi-Abedah3 <bomaged1@gmail.com>
2024-01-13 20:52:06 +0300

    adding user registraion and authentication system using django-allauth

---
+++ lushlyrics-webapp-django-main/main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'main'
+
+urlpatterns = [
+    path("", views.default, name='default'),
+    path("playlist/", views.playlist, name='your_playlists'),
+    path("search/", views.search, name='search_page'),
+]
